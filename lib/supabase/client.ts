754f3d61bb10e65961e845acd4df0c4be1c0259e
takamaro111 <takahide514370@gmail.com>
2025-07-04 09:17:54 +0900

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
   export function createClient() {
     const supabaseUrl = 'https://drhcinwdqwcosbjxwzfl.supabase.co'
-    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyaGNpbndkcXdjb3NianF3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4OTQ4ODEsImV4cCI6MjA1MTQ3MDg4MX0.aOLgJdBKxI_b7YLsZdOz7WnxqRjpgB9tgJ9DhFHrMVQ'
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyaGNpbndkcXdjb3Nianh3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMDIwNzQsImV4cCI6MjA2Njc3ODA3NH0.YfWoZPNAzI4PQIpPjEWd0XTP0IKv7wt2_vQX5pTzKv0'
 
     return createBrowserClient<Database>(
       supabaseUrl,
